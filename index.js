0f4137430d8db6723b5905a0f3e00ba7002494d8
dierat <diedra.rater@gmail.com>
2015-11-24 22:42:41 -0800

    groups rows based on participant type

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-// original spreadsheet
+// original spreadsheet, not published to the web
 var codepactSpreadsheetID = "13ywxG0Jq-eX-07mffPsPpfz1ZP8WQZu4qZ8YuVX0ecM";
 // copy, which has been published to the web
 var codepactSpreadsheetCopyID = "17DYs_snLxkW2mCYotkJe4IpRvI4i9A0kBbkhNKvi3nc";
 
 var url = "https://spreadsheets.google.com/feeds/list/" + codepactSpreadsheetCopyID + "/1/public/values?alt=json";
 
-
 $(function() {
   $.getJSON(url, function(data) {
-    
+
+    // grab all the data
     var entry = data.feed.entry;
     console.log("entry = ", entry);
 
+    // set up column headers
     $header = $('<tr></tr>');
     var columnHeaders = [];
-    // loop over example row to get column headers
     for (var key in entry[0]){
       if (key.indexOf('gsx$') > -1){
-        // save the header to loop over later
         var header = key.slice(4);
         columnHeaders.push(header);
         $header.append($('<th>' + header + '</th>'));
@@ -25,14 +24,28 @@ $(function() {
     }
     $('#agenda').append($header);
 
+    // set up participant type groups
+    $Leerkrachten = $('<tbody></tbody>');
+    $Kinderen = $('<tbody></tbody>');
+    $Ouders = $('<tbody></tbody>');
+
+    // set up each row and add it to its respective group
     $(entry).each(function(){
       var self = this;
       $row = $('<tr></tr>');
       columnHeaders.forEach(function(columnHeader){
         $row.append('<td>' + self[ 'gsx$' + columnHeader ].$t + '</td>');
       });
-      $('#agenda').append($row);
+      var doelgroep = this.gsx$doelgroep.$t;
+      if (doelgroep === 'Leerkrachten') $Leerkrachten.append($row);
+      if (doelgroep === 'Kinderen') $Kinderen.append($row);
+      if (doelgroep === 'Ouders') $Ouders.append($row);
     });
+
+    // attach the groups to the table
+    $('#agenda').append($Leerkrachten);
+    $('#agenda').append($Kinderen);
+    $('#agenda').append($Ouders);
 
   });
 });
