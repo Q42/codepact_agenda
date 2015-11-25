57b99c41764902963374895c46d85f4650c3dcfa
dierat <diedra.rater@gmail.com>
2015-11-25 10:16:37 -0800

    removes column headers on page and hardcodes columns array for iteration

--- index.js
+++ index.js
@@ -5,6 +5,19 @@ var codepactSpreadsheetCopyID = "17DYs_snLxkW2mCYotkJe4IpRvI4i9A0kBbkhNKvi3nc";
 
 var url = "https://spreadsheets.google.com/feeds/list/" + codepactSpreadsheetCopyID + "/1/public/values?alt=json";
 
+var columns = [
+  "datum",
+  "tijd",
+  "datumvisueel",
+  "naamevenement",
+  "partner",
+  "plaats",
+  "afbeeldingurl",
+  "link",
+  "doelgroep",
+  "beschrijving"
+];
+
 $(function() {
   $.getJSON(url, function(data) {
 
@@ -12,23 +25,11 @@ $(function() {
     var entry = data.feed.entry;
     console.log("entry = ", entry);
 
-    // set up column headers
-    $header = $('<tr></tr>');
-    var columnHeaders = [];
-    for (var key in entry[0]){
-      if (key.indexOf('gsx$') > -1){
-        var header = key.slice(4);
-        columnHeaders.push(header);
-        $header.append($('<th>' + header + '</th>'));
-      }
-    }
-    $('#agenda').append($header);
-
     // set up each row and add it to its respective group
     $(entry).each(function(){
       var self = this;
       $row = $('<tr></tr>');
-      columnHeaders.forEach(function(columnHeader){
+      columns.forEach(function(columnHeader){
         $row.append('<td>' + self[ 'gsx$' + columnHeader ].$t + '</td>');
       });
       var doelgroep = this.gsx$doelgroep.$t;
