b113a13a3c361a93db7a1161331198e016e1745b
dierat <diedra.rater@gmail.com>
2015-11-27 10:16:19 -0800

    switches to url of the real spreadsheet, not a copy

--- index.js
+++ index.js
@@ -1,9 +1,4 @@
-// original spreadsheet, not published to the web
-var codepactSpreadsheetID = "13ywxG0Jq-eX-07mffPsPpfz1ZP8WQZu4qZ8YuVX0ecM";
-// copy, which has been published to the web
-var codepactSpreadsheetCopyID = "17DYs_snLxkW2mCYotkJe4IpRvI4i9A0kBbkhNKvi3nc";
-
-var url = "https://spreadsheets.google.com/feeds/list/" + codepactSpreadsheetCopyID + "/1/public/values?alt=json";
+var url = "https://spreadsheets.google.com/feeds/list/13ywxG0Jq-eX-07mffPsPpfz1ZP8WQZu4qZ8YuVX0ecM/1/public/values?alt=json";
 
 $(function() {
   /******************************
